22860914d64cd5e371b0560f17cf27ccb1e2010f
ya01in <yaolin45040@gmail.com>
2025-02-08 16:23:05 +0800

    Create bookInstance adminClass

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -6,7 +6,7 @@ from .models import Author, Book, BookInstance, Genre, Language
 # admin.site.register(Book)
 # admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
 
 # Define the admin class
@@ -19,4 +19,10 @@ admin.site.register(Author, AuthorAdmin)
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
     pass
