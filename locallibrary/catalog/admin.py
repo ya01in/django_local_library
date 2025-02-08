b3ace6c0dc3cd692f0252620cc8340f5c6bd3e8a
ya01in <yaolin45040@gmail.com>
2025-02-08 16:22:29 +0800

    Create book adminClass

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Author, Genre, Book, BookInstance, Language
+
+from .models import Author, Book, BookInstance, Genre, Language
 
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
 # admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
@@ -14,3 +15,8 @@ class AuthorAdmin(admin.ModelAdmin):
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
