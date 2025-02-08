1013fcf0e73f2cd670c2e87f2ce796cad709cb91
ya01in <yaolin45040@gmail.com>
2025-02-08 16:21:24 +0800

    create auther adminClass

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,7 +3,14 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(Language)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
